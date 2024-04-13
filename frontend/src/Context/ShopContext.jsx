c67d395f06eae5155c3ec11f43e6aabc585f5b52
Anmol Tripathi <145045595+AnmolTripathii@users.noreply.github.com>
2024-04-13 23:15:31 +0530

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -159,8 +159,8 @@ const ShopContextProvider = (props) => {
             .then((data) => console.log(data))
             .catch((error) => console.error('Error adding to cart:', error));
         } else {
-            // window.location.replace("/login");
-          alert("You are not logged in. Please log in to access this feature.");
+            window.location.replace(`https://e-commerce-azfe.vercel.app/login`);
+        
         }
     }
 
